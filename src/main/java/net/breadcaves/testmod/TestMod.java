a4bcb74c7d7dcb4c1c15bb06e9a5e2d337d6ccbf
ItzDexCraft <68748056+ItzDexCraft@users.noreply.github.com>
2024-04-28 16:44:00 -0400

    Updated to support custom items.

--- src/main/java/net/breadcaves/testmod/TestMod.java
+++ src/main/java/net/breadcaves/testmod/TestMod.java
@@ -1,15 +1,16 @@
 package net.breadcaves.testmod;
 
 import com.mojang.logging.LogUtils;
+import net.breadcaves.testmod.item.ModCreativeModeTabs;
+import net.breadcaves.testmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -25,6 +26,10 @@ public class TestMod
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModCreativeModeTabs.register(modEventBus);
+
+        ModItems.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
@@ -36,7 +41,9 @@ public class TestMod
     }
 
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.TEST);
+        }
     }
 
     @SubscribeEvent
---
+++ src/main/java/net/breadcaves/testmod/item/ModCreativeModeTabs.java
@@ -0,0 +1,29 @@
+package net.breadcaves.testmod.item;
+
+import net.breadcaves.testmod.TestMod;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TestMod.MOD_ID);
+
+
+    public static final RegistryObject<CreativeModeTab> TEST_TAB = CREATIVE_MODE_TABS.register("test_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.TEST.get()))
+                    .title(Component.translatable("creativetabb.test_tab"))
+                    .displayItems((pParameters, pOutput) -> {
+                        pOutput.accept(ModItems.TEST.get());
+                    })
+                    .build());
+
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
